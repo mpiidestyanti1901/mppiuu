4d9e9457e524103083c1a0a8f4029bd1af2ec76f
mpiidestyanti1901 <113911734+mpiidestyanti1901@users.noreply.github.com>
2022-09-19 21:13:50 +0700

    Update nama_1.cpp

--- nama_1.cpp
+++ nama_1.cpp
@@ -1,1 +1,11 @@
-# mppiuu
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "NAMA : GUNTUR "<< endl;
+    cout << "KELAS : X RPL 2"<< endl;
+    cout << " NILAI : 85 "<< endl;
+}
