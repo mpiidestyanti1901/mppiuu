7dd7183c011d8ecead5727ba99ddbefd3ab866c5
mpiidestyanti1901 <113911734+mpiidestyanti1901@users.noreply.github.com>
2022-09-19 21:20:50 +0700

    Update nama_4.cpp

--- nama_4.cpp
+++ nama_4.cpp
@@ -1,1 +1,28 @@
-# mppiuu
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+   int p,l,t,v; // Deklarasi variabel panjang,lebar,tinggi,volume
+    string dimensi;
+
+    cout<<"PROGRAM C++ MENGHTUNG VOLUME BALOK"<<endl;
+    cout<<"----------------------------------"<<endl;
+    //Menginput nilai panjang,lebar dan tinggi
+    cout<<"Masukan Panjang : 18 "; 
+    cin>>p;
+    cout<<"Masukan Lebar   : 10 ";
+    cin>>l;
+    cout<<"Masukan Tinggi  : 13 ";
+    cin>>t;
+    cout<<"Masukan Satuan (mm,cm,m) Dimensi Balok : ";
+    cin>>dimensi;
+    cout<<endl;
+    
+    v=p*l*t; //Rumus untuk menghitung volume balok
+    
+    cout<<"Volume Balok    = "<<v<<" "<<dimensi<<"3"; //Tampilkan hasil
+    return 0;
+}
