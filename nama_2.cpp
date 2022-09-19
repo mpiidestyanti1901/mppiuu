2e3150f558961195a75e46615fcd2ca546207b1b
mpiidestyanti1901 <113911734+mpiidestyanti1901@users.noreply.github.com>
2022-09-19 21:16:10 +0700

    Update nama_2.cpp

--- nama_2.cpp
+++ nama_2.cpp
@@ -1,1 +1,10 @@
-# mppiuu
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "NAMA :SITI PUTRI DESTYANTI"<< endl;
+    cout << "UMUR : 15 TAHUN"<< endl; 
+}
