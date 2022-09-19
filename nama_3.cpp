d238885f86d96b18ad7dbf4c6cbbefe5e443db60
mpiidestyanti1901 <113911734+mpiidestyanti1901@users.noreply.github.com>
2022-09-19 21:18:24 +0700

    Update nama_3.cpp

--- nama_3.cpp
+++ nama_3.cpp
@@ -1,1 +1,16 @@
-# mppiuu
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+    int r;
+    float phi=3.14,luas;
+
+    cout<<"masukan jari jari lingkaran : ";
+    cin>>r;
+
+    luas=phi*r*r;
+    cout<<"luas lingkaran adalah "<<luas;
+    return 0;
+}
